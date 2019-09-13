b0ed20f379eefb24be1e5e2257c0961a9b3a118c
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-13 14:20:16 -0500

    More things

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,9 @@
 print("Hello World")
 print("This is me")
 print(5+4)
+print("5+4")
+score = 120
+score+=1
+print("Your score is", score)
+print("I want to print a double quote \n But I won't right now")  #exit code
 
