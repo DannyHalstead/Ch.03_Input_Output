a53e43a7b85501a63ef06dd0e1215964bedf597e
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-16 14:05:26 -0500

    Hello.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,12 +1,4 @@
-print("Hello World")
-print("This is me")
-print(5+4)
-print("5+4")
-score = 120
-score+=1
-print("Your score is", score)
-print("I want to print a double quote \n But I won't right now")  #exit code
-
-firstname = input("What is your favorite type of fish? ")
-lastname = input("What is your last name? ")
-print("Nice to meet you,", firstname,lastname)
+length=int(input("What is the length of the rectangle?"))
+width=int(input("What is the width of the rectangle?"))
+area = length*width
+print("The area of the rectangle is",area)
