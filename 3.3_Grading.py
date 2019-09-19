f1fec9f6219511c014311f9bff31d31507757056
halsteaddan <halsteaddan@urbandaleschools.com>
2019-09-19 13:49:02 -0500

    GradingProgram.

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -12,3 +12,11 @@ Sem Grade: 86   Final Exam: 52   Exam worth: 15%    Overall: 80.9
 Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
+
+SemesterGrade=float(input("What is your semester grade?"))
+FinalExamScore=float(input("What did you score on the final?"))
+FinalExamWorth=float(input("What percent of your grade is the final worth?"))
+
+FinalWeighted=FinalExamWorth/100
+GradeThatCounts = SemesterGrade*(1-FinalWeighted)+FinalExamScore*(FinalWeighted)
+print("Your grade that counts is", GradeThatCounts)
